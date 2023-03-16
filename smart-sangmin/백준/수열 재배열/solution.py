54d4edc94ab4a9624fa201a5114c4748135318fa
sangminnim <sangmins930@gmail.com>
2023-03-17 01:02:35 +0900

    230316_벡준_수열 재배열

--- smart-sangmin/백준/수열 재배열/solution.py
+++ smart-sangmin/백준/수열 재배열/solution.py
@@ -5,53 +5,29 @@
 7 3
 3 2 1 7 4 5 6
 """
-
-
-def check_left(start, end):
-    cnt = 1  # 자기 자신 한 개
-    for i in range(start, end):
-        if nums[end] > nums[i]:
-            cnt += 1
-    for i in range(end + 1, N):
-        if nums[end] > nums[i]:
-            break
-        cnt += 1
-    return cnt
-
-
-def check_right(start, end):
-    cnt = 1  # 자기 자신 한 개
-    for i in range(start, end):
-        if nums[start - 1] < nums[i]:
-            cnt += 1
-    for i in range(start - 2, -1, -1):
-        if nums[start - 1] < nums[i]:
-            break
-        cnt += 1
-    return cnt
-
-
 N, K = map(int, input().split())
-nums = list(map(int, input().split()))
-answer = -1
-start, end = 0, K
-while end <= N:
-    if start == 0:
-        left_cnt = check_left(start, end)
-        answer = max(left_cnt, answer)
-        print(left_cnt, -1)
-    elif end == N:
-        right_cnt = check_right(start, end)
-        answer = max(right_cnt, answer)
-        print(-1, right_cnt)
-    else:
-        left_cnt = check_left(start, end)
-        right_cnt = check_right(start, end)
-        if right_cnt + left_cnt - K == N:
-            answer = N
-            break
-        answer = max(right_cnt, left_cnt, answer)
-        print(left_cnt, right_cnt)
-    start += 1
-    end += 1
+nums = [2001] + list(map(int, input().split())) + [0]
+order_cnt = [0] + [1] * N + [0]
+rev_order_cnt = [0] + [1] * N + [0]
+
+for i in range(2, N + 1):
+    if nums[i] > nums[i - 1]:
+        order_cnt[i] += order_cnt[i - 1]
+    if nums[-i] > nums[-i - 1]:
+        rev_order_cnt[-i - 1] += rev_order_cnt[-i]
+
+answer = K
+for index in range(1, N - K + 2):
+    greater = less = 0
+    for i in range(K):
+        greater += 1 if nums[index - 1] < nums[index + i] else 0
+        less += 1 if nums[index + K] > nums[index + i] else 0
+    answer = max(
+        answer,
+        greater + order_cnt[index - 1],
+        less + rev_order_cnt[index + K],
+        order_cnt[index - 1] + K + rev_order_cnt[index + K]
+        if K == greater == less
+        else -1,
+    )
 print(answer)
