a51f3cf7a82adb76090a55095bc066fdbc7c8d40
sangminnim <sangmins930@gmail.com>
2023-03-21 20:15:08 +0900

    230321_백준_인구이동

--- smart-sangmin/백준/오목/오목.java
+++ smart-sangmin/백준/오목/오목.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class 오목 {
     static int[][] board;
 
     public static void main(String[] args) throws IOException {
